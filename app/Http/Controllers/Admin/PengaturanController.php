0805868658a31f5d1bc78c0f41d976c84ca4b220
Muhammad Izza Alfiansyah <iansyah724@gmail.com>
2023-08-24 12:34:02 +0700

    make page for pengaturan admin

--- app/Http/Controllers/Admin/PengaturanController.php
+++ app/Http/Controllers/Admin/PengaturanController.php
@@ -3,12 +3,15 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Pengaturan;
 use Illuminate\Http\Request;
 
 class PengaturanController extends Controller
 {
     function index()
     {
-        return view('admin.pengaturan.index');
+        $pengaturan = Pengaturan::first();
+
+        return view('admin.pengaturan.index', compact('pengaturan'));
     }
 }
