5638295e3799a4b202f816ee4d77feeb80e210d6
Muhammad Izza Alfiansyah <iansyah724@gmail.com>
2023-08-25 19:10:07 +0700

    check if database using custom email

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -6,6 +6,7 @@ use App\Models\Pengaturan;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Support\Facades\Config;
 use Illuminate\Support\Facades\View;
 
 class Controller extends BaseController
@@ -16,5 +17,19 @@ class Controller extends BaseController
     {
         $pengaturan = Pengaturan::find('1');
         View::share('app_pengaturan', $pengaturan);
+
+        if ($pengaturan?->mail_username) {
+            $config = array(
+                'driver'     => 'smtp',
+                'host'       => 'smtp.gmail.com',
+                'port'       => '465',
+                'from'       => env('MAIL_FROM_ADDRESS'),
+                'encryption' => 'ssl',
+                'username'   => $pengaturan?->mail_username,
+                'password'   => $pengaturan?->mail_password,
+            );
+
+            Config::set('mail', $config);
+        }
     }
 }
