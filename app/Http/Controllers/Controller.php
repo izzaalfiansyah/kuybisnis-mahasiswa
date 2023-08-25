57c0a3e59b833ead3185e3ed72eff6654596b3e5
Muhammad Izza Alfiansyah <iansyah724@gmail.com>
2023-08-25 19:57:03 +0700

    change mail config

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -18,15 +18,15 @@ class Controller extends BaseController
         $pengaturan = Pengaturan::find('1');
         View::share('app_pengaturan', $pengaturan);
 
+        Config::set('mail.from.address', 'admin@kuybisnis.com');
+        Config::set('mail.from.name', 'KuyBisnis');
+
         if ($pengaturan?->mail_username) {
-            Config::set('mail.driver', 'smtp');
-            Config::set('mail.mailer', 'smtp');
             Config::set('mail.host', 'smtp.gmail.com');
             Config::set('mail.port', '465');
-            Config::set('mail.encryption', 'ssl');
-            Config::set('mail.from.address', 'admin@kuybisnis.com');
             Config::set('mail.username', $pengaturan->mail_username);
-            Config::set('mail.username', $pengaturan->mail_password);
+            Config::set('mail.password', $pengaturan->mail_password);
+            Config::set('mail.encryption', 'ssl');
         }
     }
 }
