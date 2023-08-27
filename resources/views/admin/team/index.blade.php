cbb77ddc256b3bfebce4bbadf52eea02180e5c17
Muhammad Izza Alfiansyah <iansyah724@gmail.com>
2023-08-27 09:56:34 +0700

    show list team

--- resources/views/admin/team/index.blade.php
+++ resources/views/admin/team/index.blade.php
@@ -5,30 +5,35 @@
                 <div class="card-title">Data Tim</div>
                 <a href="{{ route('admin.team.create') }}" class="btn btn-primary">Tambah</a>
             </div>
-            <table class="table table-lg">
-                <thead>
-                    <tr>
-                        <th></th>
-                        <th>Nama</th>
-                        <th>Jabatan</th>
-                        <th>Opsi</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    @forelse ($team as $item)
+            <div class="overflow-x-auto">
+                <table class="table table-lg">
+                    <thead>
                         <tr>
-                            <td>{{ $item->foto }}</td>
-                            <td>{{ $item->nama }}</td>
-                            <td>{{ $item->jabatan }}</td>
-                            <td></td>
+                            <th></th>
+                            <th>Nama</th>
+                            <th>Jabatan</th>
+                            <th>Opsi</th>
                         </tr>
-                    @empty
-                        <tr>
-                            <td colspan="4" class="text-center">tim belum tersedia</td>
-                        </tr>
-                    @endforelse
-                </tbody>
-            </table>
+                    </thead>
+                    <tbody>
+                        @forelse ($team as $item)
+                            <tr>
+                                <td>
+                                    <img src="{{ asset($item->foto) }}" alt=""
+                                        class="rounded-full h-12 w-12 object-cover">
+                                </td>
+                                <td>{{ $item->nama }}</td>
+                                <td>{{ $item->jabatan }}</td>
+                                <td></td>
+                            </tr>
+                        @empty
+                            <tr>
+                                <td colspan="4" class="text-center">tim belum tersedia</td>
+                            </tr>
+                        @endforelse
+                    </tbody>
+                </table>
+            </div>
         </div>
     </div>
 </x-app-layout>
