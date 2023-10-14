a69f46cd9631d941b4edfb054d2e1da4fa9cdd9c
Muhammad Izza Alfiansyah <iansyah724@gmail.com>
2023-10-14 09:05:08 +0700

    add badge validated at laporan

--- resources/views/user/laporan/index.blade.php
+++ resources/views/user/laporan/index.blade.php
@@ -128,6 +128,20 @@
                                 class="w-[250px] lg:ml-[30px] ml-0 mb-[30px] {{ $hasil['medal'] == 'nope' ? 'mt-6' : '' }}">
                             <div class="card-body lg:flex-1 w-full flex items-center justify-center">
                                 <div class="w-full text-justify">
+                                    @if ($kelompok->status == '1')
+                                        <div class="flex flex-row">
+                                            <span
+                                                class="mb-4 flex bg-green-500 rounded px-3 py-0.5 items-center space-x-2 text-white">
+                                                <svg xmlns="http://www.w3.org/2000/svg" fill="none"
+                                                    viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
+                                                    class="w-6 h-6 stroke-2">
+                                                    <path stroke-linecap="round" stroke-linejoin="round"
+                                                        d="M9 12.75L11.25 15 15 9.75M21 12c0 1.268-.63 2.39-1.593 3.068a3.745 3.745 0 01-1.043 3.296 3.745 3.745 0 01-3.296 1.043A3.745 3.745 0 0112 21c-1.268 0-2.39-.63-3.068-1.593a3.746 3.746 0 01-3.296-1.043 3.745 3.745 0 01-1.043-3.296A3.745 3.745 0 013 12c0-1.268.63-2.39 1.593-3.068a3.745 3.745 0 011.043-3.296 3.746 3.746 0 013.296-1.043A3.746 3.746 0 0112 3c1.268 0 2.39.63 3.068 1.593a3.746 3.746 0 013.296 1.043 3.746 3.746 0 011.043 3.296A3.745 3.745 0 0121 12z" />
+                                                </svg>
+                                                <span class="text-sm font-semibold">Tervalidasi</span>
+                                            </span>
+                                        </div>
+                                    @endif
                                     {{ $result['message'] }}
                                 </div>
                             </div>
